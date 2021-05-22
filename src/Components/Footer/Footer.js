d000899503f619e4315c617665a5e949387834d6
joelshaw <shawji@me.com>
2021-05-21 22:29:20 -0500

    init existing react work

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div>
+      <footer id="footer">
+      <div class="site-container">
+			  <div class="site-info">
+          <p>&copy; 2021 Joel Shaw Photography. All Rights Reserved.</p>
+          <p><a href="https://www.twelfthjames.com/" rel="noreferrer" target="_blank">A Twelfth &amp; James Company</a></p>
+        </div>
+		  </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <div>
+      <section id="header">
+        <img src="https://joelshaw.nyc3.cdn.digitaloceanspaces.com/assets/js-photog-logo.png" alt="" />
+      </section>
+    </div>
+  );
+}
+
+export default Header;
